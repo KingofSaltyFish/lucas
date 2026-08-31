8b2f4e1b715a8db2e42434b28f4720f3481de49a
KingofSaltyFish <lucasmahjkl@gmail.com>
2026-08-31 12:01:52 +0800

    Add live demos and English interface

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import GitHubProjects from './GitHubProjects.vue'
+import DemoFrame from './DemoFrame.vue'
+import KalmanDemo from './KalmanDemo.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('GitHubProjects', GitHubProjects)
+    app.component('DemoFrame', DemoFrame)
+    app.component('KalmanDemo', KalmanDemo)
   }
 }
